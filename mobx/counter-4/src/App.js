5abcbc44282006cdb910fdfeafc29620301c1519
Miklos Bertalan <miklos.bertalan@risingstack.com>
2018-10-06 01:43:15 +0200

    fix mobx decorators example

--- mobx/counter-4/src/App.js
+++ mobx/counter-4/src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import { observer, inject } from 'mobx-react';
+import React, { Component } from 'react'
+import { observer, inject } from 'mobx-react'
 
 @inject('counter')
 @observer
-export default class App extends Component {
+class App extends Component {
   render() {
-    const { counter } = this.props;
+    const { counter } = this.props
     return (
       <div>
         <p>The value is: {counter.value}</p>
         <button onClick={counter.increment}>Increment</button>
         <button onClick={counter.decrement}>Decrement</button>
       </div>
-    );
+    )
   }
 }
+
+export default App
