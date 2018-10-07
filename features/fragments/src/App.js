9f343f398bd5fc48f8dceef6a163c04f2909cd57
Miklos Bertalan <miklos.bertalan@risingstack.com>
2018-10-07 10:25:56 +0200

    fix fragments example

--- features/fragments/src/App.js
+++ features/fragments/src/App.js
@@ -1,37 +1,22 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import React from 'react'
+import styled from 'styled-components'
+import UserMenus from './UserMenus'
 
-const Login = () => <h1>Login</h1>
-const Admin = () => <h1>Admin</h1>
-const Timeline = () => <h1>Timeline</h1>
-const NotFound = () => <h1>404!</h1>
+const NavBar = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
 
-export default class App extends Component {
-  state = {}
-
-  toggleLogin = () => this.setState({ isLoggedIn: !this.state.isLoggedIn })
-
-  render() {
-    const { isLoggedIn } = this.state
-
-    return (
-      <>
-        <button onClick={this.toggleLogin}>Toggle Login</button>
-
-        <Router>
-          <Switch>
-            {!isLoggedIn ? (
-              <Route path="/" component={Login} />
-            ) : (
-              <>
-                <Route exact path="/" component={Timeline} />
-                <Route exact path="/admin" component={Admin} />
-              </>
-            )}
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </Router>
-      </>
-    )
+  > *:not(:first-child) {
+    margin-left: 10px;
   }
+`
+
+export default function App() {
+  return (
+    <NavBar>
+      <div>Home page</div>
+      <UserMenus />
+    </NavBar>
+  )
 }
---
+++ features/fragments/src/UserMenus.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+
+export default function Items() {
+  return (
+    <Fragment>
+      <div>Log out</div>
+      <div>Settings</div>
+    </Fragment>
+  )
+}
